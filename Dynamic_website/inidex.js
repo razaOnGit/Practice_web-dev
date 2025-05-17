76946c3c48411e0de3b63546cd837eea7bd6f604
MD RAZA KHAN <kmdraza47@gmail.com>
2025-05-17 19:42:38 +0530

    Dynamic_website

---
+++ Dynamic_website/inidex.js
@@ -0,0 +1,41 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+    let viewStr
+    if (views < 1000) {
+        viewStr = views;
+    }
+    else if (views > 1000000) {
+        viewStr = views / 1000000 + "M";
+    }
+    else {
+        viewStr = views / 1000 + "K";
+    }
+
+
+    let videoAge
+    if(monthsOld<12){
+        videoAge = monthsOld + "months";
+    }
+    else if(monthsOld>12){
+        videoAge = monthsOld%12 + "years";
+    }
+
+    let html = `<div class="card">
+    <div class="image">
+        <img src="${thumbnail}"
+            alt="">
+        <div class="capsule">${duration}</div>
+    </div>
+    <div class="text">
+        <h1>${title}</h1>
+        <p>${cName} . ${viewStr} views . ${videoAge}  ago</p>
+    </div> 
+</div>`
+
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+}
+
+
+createCard("Introduction to Backend | Sigma Web Dev video #2", "CodeWithHarry", 560000, 7, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")  
+
+createCard("Introduction to raza profile | Sigma Web Dev video #2", "CodeWithHarry", 860000, 14, "31:22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw")  
